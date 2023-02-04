a3e4bcab578313b63e0ce60b1610b8f319cc2185
ocatten <u0580588@umail.utah.edu>
2023-02-03 22:11:37 -0700

    fixed it all

--- CS 2420/src/assign03/PriorityQueueTester.java
+++ CS 2420/src/assign03/PriorityQueueTester.java
@@ -1,7 +1,11 @@
 package assign03;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,40 +13,134 @@ import org.junit.jupiter.api.Test;
 
 public class PriorityQueueTester {
 
-	private SimplePriorityQueue simpleEmptyPriorityQueue;
-	private SimplePriorityQueue simpleSmallPriorityQueue;
+	private SimplePriorityQueue emptySimplePriorityQueue;
+	private SimplePriorityQueue smallSimplePriorityQueue;
+	private ArrayList<String> jumbledLetters = new ArrayList<String>();
 	
 	@BeforeEach
 	void setUp() throws Exception {
-		String[] simpleStringArray = new String[]{"A","B","C"};
 		
-		simpleEmptyPriorityQueue = new SimplePriorityQueue();
-		W
-		simpleSmallPriorityQueue = new SimplePriorityQueue();
-		simpleSmallPriorityQueue.data = simpleStringArray;
+		emptySimplePriorityQueue = new SimplePriorityQueue();
 		
+		smallSimplePriorityQueue = new SimplePriorityQueue();
+		smallSimplePriorityQueue.data = new String[] {"A", "B", "C", "F", "H"};
 		
-		
+		String[] jumbledArray = new String[] {"O", "M", "J", "G", "K", "D", "I", "L", "N", "P"};
+		for(int i = 0; i < jumbledArray.length; i++) {
+			jumbledLetters.add(jumbledArray[i]);
+		}
 	}
 	
-	@Test
-	public void testFindMax() {
-
-	}
 	
 	@Test
-	public void testInsertWithEmptyList() {
+	public void testsmallListInsert() {
 		
-		simpleEmptyPriorityQueue.insert("D");
-		//System.out.println(simplePriorityQueue.data[0]);
-		assertEquals("D", simpleEmptyPriorityQueue.data[0]);
+		smallSimplePriorityQueue.insert("I");
+		for(int i = 0; i < smallSimplePriorityQueue.data.length; i++) {
+			System.out.print(smallSimplePriorityQueue.data[i] + " ");
+		}	
+		
+		System.out.println("");
+		assertEquals( smallSimplePriorityQueue.data[5], "I" );
+		
+		
+		smallSimplePriorityQueue.insert("D");
+		for(int i = 0; i < smallSimplePriorityQueue.data.length; i++) {
+			System.out.print(smallSimplePriorityQueue.data[i] + " ");
+		}	
+		
+		System.out.println("");
+		assertEquals( smallSimplePriorityQueue.data[3], "D" );
+		
+		
+		smallSimplePriorityQueue.insert("C");
+		for(int i = 0; i < smallSimplePriorityQueue.data.length; i++) {
+			System.out.print(smallSimplePriorityQueue.data[i] + " ");
+		}
+		
+		System.out.println("");
+		assertEquals( smallSimplePriorityQueue.data[3], "C" );
+		
+		
+		smallSimplePriorityQueue.insert("G");
+		for(int i = 0; i < smallSimplePriorityQueue.data.length; i++) {
+			System.out.print(smallSimplePriorityQueue.data[i] + " ");
+		}	
+		System.out.println("");
+		assertEquals( smallSimplePriorityQueue.data[6], "G" );
 	}
 	
+	
+	
 	@Test
-	public void testInsertWithSmallList() {
+	public void testEmptyInsert() {
 		
-		simpleEmptyPriorityQueue.insert("D");
-		//System.out.println(simplePriorityQueue.data[0]);
-		assertEquals("D", simpleEmptyPriorityQueue.data[0]);
+		emptySimplePriorityQueue.insert("A");
+		assertEquals( emptySimplePriorityQueue.data[0], "A" );
+		
+		emptySimplePriorityQueue.insert("B");
+		assertEquals( emptySimplePriorityQueue.data[1], "B" );
+	}
+	
+	
+	
+	@Test
+	public void testInsertAll() {
+		
+		smallSimplePriorityQueue.insertAll(jumbledLetters);
+		for(int i = 0; i < smallSimplePriorityQueue.data.length; i++) {
+			System.out.print(smallSimplePriorityQueue.data[i] + " ");
+		}	
+		System.out.println("");
+		
+		assertEquals( smallSimplePriorityQueue.data[0], "A" );
+		assertEquals( smallSimplePriorityQueue.data[1], "B" );
+		assertEquals( smallSimplePriorityQueue.data[2], "C" );
+		assertEquals( smallSimplePriorityQueue.data[3], "D" );
+		assertEquals( smallSimplePriorityQueue.data[4], "F" );
+		assertEquals( smallSimplePriorityQueue.data[5], "G" );
+		assertEquals( smallSimplePriorityQueue.data[6], "H" );
+		assertEquals( smallSimplePriorityQueue.data[7], "I" );
+		assertEquals( smallSimplePriorityQueue.data[8], "J" );
+		assertEquals( smallSimplePriorityQueue.data[9], "K" );
+		assertEquals( smallSimplePriorityQueue.data[10], "L" );
+		assertEquals( smallSimplePriorityQueue.data[11], "M" );
+		assertEquals( smallSimplePriorityQueue.data[12], "N" );
+		assertEquals( smallSimplePriorityQueue.data[13], "O" );
+		assertEquals( smallSimplePriorityQueue.data[14], "P" );
+	}
+	
+	
+	
+	@Test
+	public void testDeleteMax() {
+		
+		smallSimplePriorityQueue.deleteMax();
+		assertEquals(smallSimplePriorityQueue.data[ smallSimplePriorityQueue.data.length-1 ], "F");
+		smallSimplePriorityQueue.deleteMax();
+		assertEquals(smallSimplePriorityQueue.data[ smallSimplePriorityQueue.data.length-1 ], "C");
+	}
+	
+	
+	
+	@Test
+	public void testContains() {
+		
+		assertTrue(smallSimplePriorityQueue.contains("A"));
+		assertTrue(smallSimplePriorityQueue.contains("B"));
+		assertTrue(smallSimplePriorityQueue.contains("C"));
+		assertFalse(smallSimplePriorityQueue.contains("D"));
+		assertFalse(smallSimplePriorityQueue.contains("E"));
+		assertTrue(smallSimplePriorityQueue.contains("F"));
+	}
+	
+	
+	
+	
+	@Test
+	public void testClear() {
+		
+		smallSimplePriorityQueue.clear();
+		assertTrue(smallSimplePriorityQueue.isEmpty());
 	}
 }
--- CS 2420/src/assign03/SimplePriorityQueue.java
+++ CS 2420/src/assign03/SimplePriorityQueue.java
@@ -4,27 +4,23 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.NoSuchElementException;
 
-import assign02.CS2420StudentGeneric;
-
-public class SimplePriorityQueue<E> implements PriorityQueue<E> 
-{
+public class SimplePriorityQueue<E> implements PriorityQueue<E> {
+	
 	E[] data = (E[]) new Object[0];
 	
 	/**
 	 * Simple Priority Queue constructor
 	 */
-	public SimplePriorityQueue() {
-
-	}
+	public SimplePriorityQueue() { }
 	
 	/**
 	 * Simple Priority Queue constructor that will sort
 	 * the array via comparator
 	 */
-	public SimplePriorityQueue(Comparator<? super E> cmp) {
-		
-	}
+	public SimplePriorityQueue(Comparator<? super E> cmp) { }
 
+	
+	
 	/**
 	 * Retrieves, but does not remove, the maximum element in this priority
 	 * queue.
@@ -33,9 +29,17 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E>
 	 * @throws NoSuchElementException if the priority queue is empty
 	 */
 	public E findMax() throws NoSuchElementException {
+		
+		// Catch case for an empty list
+		if(data.length == 0) {	
+			throw new NoSuchElementException();
+		}
+		
 		return data[data.length-1];
 	}
 
+	
+	
 	/**
 	 * Retrieves and removes the maximum element in this priority queue.
 	 * 
@@ -44,10 +48,16 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E>
 	 */
 	public E deleteMax() throws NoSuchElementException {
 		
+		// Catch case for an empty list
+		if(data.length == 0) {	
+			throw new NoSuchElementException();
+		}
+		
+		
 		E max = data[data.length-1];
 		
 		//Create new array without the max variable
-		E[] dataDeleted =  (E[]) new Object[data.length-2];
+		E[] dataDeleted =  (E[]) new Object[data.length-1];
 		for(int i = 0; i < data.length - 1; i++) {
 			dataDeleted[i] = data[i];
 		}
@@ -58,54 +68,101 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E>
 		return max;
 	}
 
+	
+	
 	/**
 	 * Inserts the specified element into this priority queue.
 	 * 
 	 * @param item - the element to insert
 	 */
 	public void insert(E item) {
-		//Find location in array to place item
+		
+		// Create new list thats one larger than the current list to 
+		// place the new item in.
+		E[] newDataArray =  (E[]) new Object[data.length + 1];
+		
+		
+		// Find location in array to place item
 		int addedIndex = binarySearch(item);
+		//System.out.println("addedIndex: " + addedIndex); // Shows what it's adding specifically
 		
-		
-		//Create new list thats one larger than the current list to 
-		//place the new item in.
-		E[] dataAdded =  (E[]) new Object[data.length];
+		// Creates a flag that notifies the algorithm if the new element has been added for tracking.
 		boolean ifAdded = false;
-		
-		//If array is empty, create new array with just the item
-		if( data.length == 0 )
-		{
-			//System.out.println("empty array"); // Test statement
-			dataAdded = (E[]) new Object[] {item};
-		}
 	
-		for(int i = 0; i < data.length - 1; i++) {
-			if(i == addedIndex)
-			{
-				dataAdded[i] = item;
+		for(int i = 0; i < data.length + 1; i++) {
+			
+			if(i == addedIndex) {
+				
+				newDataArray[i] = item;
 				//boolean flag to show that the new item has been added
 				ifAdded = true;
 				continue;
 			}
+		
 			// If the item has not been added transfer the list over to the new
-			//list according to the corresponding index
+			//  list according to the corresponding index
 			else if(!ifAdded) {
-				dataAdded[i] = data[i];
+				newDataArray[i] = data[i];
 				continue;
 			}
 			// If the item has been added transfer the list over to the new
 			//list according to the previous index
-			else{
-				dataAdded[i] = data[i-1];
+			else {
+				newDataArray[i] = data[i-1];
 			}
 		}
 		
-		//Make the new list equal to the current list
-		data = dataAdded;
+		// Make the new list equal to the current list
+		data = newDataArray;
+	}
+	
+	
+	
+	/**
+	 * Helper method that does a binary search for the data array
+	 * 
+	 * @param item
+	 * @throws NoSuchElementException if the array doesn't have that index
+	 */
+	public int binarySearch(E item) throws NoSuchElementException {
+		
+		// Sets up fields for the binary search
+		int high = data.length;
+		int low = 0;
+		int mid = ((high - low) / 2) + low;
+		
+		while(low < high) {
+			
+			// Calculate the middle of the new set of data and compare the middle of the data set with the param
+			//System.out.println("mid calculated: " + mid); // Test case for out of bounds error / infinite loop case
+			int midToParam = ((Comparable<? super E>)data[mid]).compareTo(item);
+			//System.out.println("midToParam result: " + midToParam); // Typewriting the code
+			
+			// If the midpoint is below the parameter:
+			if (midToParam < 0) {
+				low = mid + 1; // Add one to the low since it starts at 0
+			}
+			
+			// If the midpoint is greater than the parameter:
+			if (midToParam > 0) {	
+				high = mid;
+			}
+			
+			// If the low midToParam is equal, return the low's index plus one
+			if (midToParam == 0) {
+				return mid + 1;
+			}
+			
+			mid = ((high - low) / 2) + low; // Recalculate the midpoint.
+		}
+		
+		//System.out.println("while loop exited");
+		// Return 0 if the high is never above the low (if the list is 0)
+		return mid;
 	}
 	
 
+	
 	/**
 	 * Inserts the specified elements into this priority queue.
 	 * 
@@ -113,59 +170,13 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E>
 	 */
 	public void insertAll(Collection<? extends E> coll) {
 		
-
-		for(E newItem: coll) {
-			//Find location in array to place item
-			int addedIndex = binarySearch(newItem);
-			
-			//Create new list thats bigger than the current length equal to the 
-			//size of the new list
-			E[] dataAdded =  (E[]) new Object[data.length + coll.size() - 1];
-			int numAdded = 0;
-			
-			/*
-			//If array is empty, create new array with just the item
-			if( data.length == 0 )
-			{
-				//System.out.println("empty array"); // Test statement
-				dataAdded = (E[]) new Object[] {item};
-			}
-			*/
-			
-			
-			for(int i = 0; i < data.length - 1; i++) {
-				dataAdded[i]
-				
-				if(i == addedIndex)
-				{
-					dataAdded[i] = item;
-					//boolean flag to show that the new item has been added
-					ifAdded = true;
-					continue;
-				}
-				// If the item has not been added transfer the list over to the new
-				//list according to the corresponding index
-				else if(!ifAdded) {
-					dataAdded[i] = data[i];
-					continue;
-				}
-				// If the item has been added transfer the list over to the new
-				//list according to the previous index
-				else{
-					dataAdded[i] = data[i-1];
-				}
-			}
+		// Loops through each item in the collection and adds it to the existing data.
+		for (E item : coll) {
+			this.insert(item);
 		}
-		
-		
-		
-		
-	
-		
-		
-		//Make the new list equal to the current list
-		data = dataAdded;
 	}
+	
+	
 
 	/**
 	 * Indicates whether this priority queue contains the specified element.
@@ -175,10 +186,10 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E>
 	public boolean contains(E item) {
 		//Loop through each item in the data array and if the item equals 
 		//the item given return true
-		for(E dataItem: data)
-		{
-			if(dataItem.equals(item))
-			{
+		for(E dataItem: data) {
+			
+			if(dataItem.equals(item)) {
+				
 				return true;
 			}
 		}
@@ -218,48 +229,6 @@ public class SimplePriorityQueue<E> implements PriorityQueue<E>
 		//equal to this new list
 		E[] dataCleared = (E[]) new Object[0];
 		data = dataCleared;		
-	}
-	
-	
-	
-	/**
-	 * Helper method that does a binary search for the data array
-	 * 
-	 * @param item
-	 * @throws NoSuchElementException if the array doesn't have that index
-	 */
-	public int binarySearch(E item) throws NoSuchElementException {
-		
-		// Catch case for an empty array
-		if( data.length == 0 )
-		{
-			return 0;
-		}
-		
-		// Variables to keep track of search parameter indexes
-		int low = 0;
-		int high = data.length - 1;
-		int mid = high/2;
-		
-		// Loop while the search window exists
-		while( low != high ) {
-			
-			// Casts the midpoint object to a comparable and compares it to the param
-			int compResult = ( (Comparable<? super E>)data[mid] ).compareTo(item);
-			
-			// If the midpoint is greater,
-			if(compResult > 0) {	
-				high = mid;
-			} else if (compResult < 0) { // If the midpoint is lower than the param
-				low = mid;
-			} else if (compResult == 0) { // If there's a match:
-				return mid;
-			}
-		}
-		
-		// If nothing is found
-		System.out.println("Search failed.");
-		return 0;
 	}
 	
 }
